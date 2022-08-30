ac70352c5d929dfe4db621d0489d99fad6087509
bae-sh <hyeon1293@gmail.com>
2022-08-30 20:54:48 +0900

    remove all

--- project/src/Routes/Menual/index.js
+++
@@ -1,7 +0,0 @@
-// 현재 라우터 index부분
-// Router.js에서 파라미터로 userInfo,setUserInfo 를 받음
-
-/* eslint-disable import/no-anonymous-default-export */
-import Container from "./Container";
-
-export default () => <Container />;
